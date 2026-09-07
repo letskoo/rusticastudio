1271b4702bb05032a3c4c6a0215fac01dacb7c0d
letskoo <kiwankoo@naver.com>
2026-09-07 19:00:40 +0900

    Release 1.0.12 update album selection text

--- src/ui/album/albumView.js
+++ src/ui/album/albumView.js
@@ -358,7 +358,7 @@ export function clearAlbumView() {
     deps.albumSelectedCount.innerHTML =
         `
             <span class="album-selected-label">
-                선택한 사진
+                인화할 사진 선택
             </span>
 
             <strong class="album-selected-number">
